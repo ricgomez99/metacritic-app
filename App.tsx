6b83caeb34f8848d4638672c979ca89b45c3ba0f
Andres <argomez.restrepo@gmail.com>
2025-04-10 15:56:01 -0500

    added first elements

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, View, Text, Button } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+const URL =
+  "https://images2.medimops.eu/product/933395/M02302056388-source.jpg";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Image style={styles.image} source={{ uri: URL }} />
+      <Button onPress={() => alert("Hello!")} title="Press here" />
+      <Text style={styles.title}>Hello World!</Text>
     </View>
   );
 }
@@ -13,8 +18,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  title: {
+    color: "#111",
+  },
+
+  image: {
+    width: 215,
+    height: 294,
+    resizeMode: "center",
   },
 });
